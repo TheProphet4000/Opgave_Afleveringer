219b3e9eb6179e571252e0d5c6d4a83cab0b4c6a
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2020-09-15 13:26:06 +0200

    Nye Opgaver lavet i Javabogen

--- Indtruduction to Java 11th edition Opgaver/src/Keywords.java
+++ Indtruduction to Java 11th edition Opgaver/src/Keywords.java
@@ -1,5 +1,5 @@
 public class Keywords {
     public static void main(String[] args) {
-        System.out.println("valide java keywords er, a++, $4, class, public, int, x og y og radius er variablenavne som er valide");
+        System.out.println("valide java keywords er, a++, --a, $4, class, public, int, x og y og radius er variablenavne som er valide");
     }
 }
