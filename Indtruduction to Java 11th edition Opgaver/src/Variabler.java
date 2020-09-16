ff791d6e824d0e60d059210ea374f78f56a3c94a
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2020-09-16 17:36:50 +0200

    Datatyper

---
+++ Indtruduction to Java 11th edition Opgaver/src/Variabler.java
@@ -0,0 +1,24 @@
+public class Variabler {
+    public static void main(String[] args) {
+
+        int Num = 47;
+        float Float = 21.42f;
+        char Letter = 'M';
+        boolean Bool = true;
+        String Text = "Hello";
+        byte Byte = 1;
+        short Short = 11;
+        double Double = 900000000;
+        long Long = 900000000;
+
+        System.out.println(Num);
+        System.out.println(Float);
+        System.out.println(Letter);
+        System.out.println(Bool);
+        System.out.println(Text);
+        System.out.println(Byte);
+        System.out.println(Short);
+        System.out.println(Double);
+        System.out.println(Long);
+    }
+}
