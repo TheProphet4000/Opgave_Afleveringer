3cb1489a544f3758ff950cb32f95865ddc7a6bac
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2020-09-30 13:22:57 +0200

    Kap 3 færdigt

--- Kapitel3/src/com/exampel/IfElseStatements/Opgaver_335.java
+++ Kapitel3/src/com/exampel/IfElseStatements/Opgaver_335.java
@@ -1,7 +1,12 @@
 package com.exampel.IfElseStatements;
+import java.util.Scanner;
 
 public class Opgaver_335 {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        int usernumber = input.nextInt();
+
+        System.out.println((usernumber % 2 == 0) ? "Number is even":"Number is odd");
     }
 }
