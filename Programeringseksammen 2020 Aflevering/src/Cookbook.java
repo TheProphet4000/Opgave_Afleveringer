3e4ac451e1443e3a48309260831ea9c4742f6ba5
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2020-12-22 10:39:32 +0100

    Opskrifter tilføjet

--- Programeringseksammen 2020 Aflevering/src/Cookbook.java
+++ Programeringseksammen 2020 Aflevering/src/Cookbook.java
@@ -6,12 +6,25 @@ enum recipies{
 public class Cookbook {
     public static void main(String[] args) {
 
-        recipies lemonTea = recipies.page1; //sætter page1 til lemonTea (Citrontee)
+        recipies lemonTea = recipies.page1; //sætter page1 til lemonTea (Citron tee)
         recipies glaze = recipies.page2; // sætter page2 til glaze (glassur)
 
-        showRecipies(); //kalder metoden showRecipies()
+        showLemonTea(lemonTea); //kalder metoden showLemonTea() der viser hvordan man laver citron tee
+        showGlaze(glaze); // det sammen som linje 12, bare med glaze.
     }
 
-    private static void showRecipies() {
+
+    private static void showLemonTea(recipies lemonTea) {
+        System.out.println("------------------------------------------Lemon tea-------------------------------------------");
+        System.out.println("To make lemon tea, add a slice of lemon to your teacup and put boiling water in the teacup.");
+        System.out.println("If the lemon taste is to sour add sugar, or add less lemon next time");
+        System.out.println("----------------------------------------------end---------------------------------------------");
+    }
+
+    private static void showGlaze(recipies glaze){
+        System.out.println("--------------------------------------------Glaze---------------------------------------------");
+        System.out.println("To make glaze, add powdered suger in a small bowl and add water, mix it with a spoon");
+        System.out.println("if to runny add more powdered sugar, if to chick add water");
+        System.out.println("----------------------------------------------end---------------------------------------------");
     }
 }
